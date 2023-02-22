97844fd87e01eb50f157cd39188d1f083a38b2ca
Tobias Geibinger <tobias.geibinger@tuwien.ac.at>
2023-02-22 22:50:54 +0100

    wip

--- cobrabench.py
+++ cobrabench.py
@@ -8,13 +8,33 @@ import json
 import sys
 import datetime
 from pathlib import Path
-from typing import Dict, List, Optional, Callable, Any
+from typing import Dict, List, Optional, Callable, Any, TypedDict
 
 DEFAULT_RUN_SOLVER_KILL_DELAY = 10
 DEFAULT_TIME_BUFFER = 1
 DEFAULT_N_MEM_LINES = 4
 DEFAULT_N_CPUS = 24
 DEFAULT_PARTITION = 'broadwell'
+
+class BenchConfig(TypedDict):
+    name: str
+    instances: str
+    configs: str
+    timeout: int
+    request_cpus: int
+    mem_limit: int
+    runs: Optional[int]
+    executable: Optional[str]
+    working_dir: Optional[str]
+    runsolver_path: Optional[str]
+    runsolver_kill_delay: Optional[int]
+    slurm_time_buffer: Optional[int]
+    timeout_factor: Optional[int]
+    initial_seed: Optional[int]
+    partition: Optional[str]
+    cpu_per_node: Optional[int]
+    mem_lines: Optional[int]
+
 
 def process_bench(bench_folder: Path, log_read_func: Callable[[Path], Optional[Dict[str, Any]]], 
                   metadata_file: Optional[str] = None) -> List[Dict[str, Any]]:
@@ -48,89 +68,36 @@ def process_bench(bench_folder: Path, log_read_func: Callable[[Path], Optional[D
     return data
 
 
-def main(cobra_config : Dict[str, str | int], bench_config : Dict[str, str | int]) -> None:
+def main(bench_config : BenchConfig) -> None:
 
-    if 'runsolver_path' in cobra_config:
-        runsolver_path = str(cobra_config['runsolver_path'])
-    else:
-        runsolver_path = "runsolver"
+    # required fields
+    bench_name = bench_config['name']
+    instances_file = bench_config['instances']
+    configs_file = bench_config['configs']
+    timeout = bench_config['timeout']
+    mem_limit = bench_config['mem_limit']
+    request_cpu = bench_config['request_cpus']
 
-    if 'mem_lines' in cobra_config:
-        mem_lines = int(cobra_config['mem_lines'])
-    else:
-        mem_lines = DEFAULT_N_MEM_LINES
-
-    if 'cpu_per_node' in cobra_config:
-        cpu_per_node = int(cobra_config['cpu_per_node'])
-    else:
-        cpu_per_node = DEFAULT_N_CPUS 
-
-    if 'partition' in cobra_config:
-        partition = cobra_config['partition']
-    else:
-        partition = DEFAULT_PARTITION
-
-    if 'name' in bench_config:
-        bench_name = str(bench_config['name'])
-    else:
-        raise ValueError('bench config is missing required field "name"')
-    if 'timeout' in bench_config:
-        timeout = int(bench_config['timeout'])
-    else:
-        raise ValueError('bench config is missing required field "timeout"')
-    
-    if 'runs' in bench_config:
-        n_runs = int(bench_config['runs'])
-    else:
-        n_runs = 1
-
-    if 'mem_limit' in bench_config:
-        mem_limit = int(bench_config['mem_limit'])
-    else:
-        raise ValueError('bench config is missing required field "mem_limit"')
-
-    if 'request_cpus' in bench_config:
-        request_cpu = int(bench_config['request_cpus'])
-    else:
-        raise ValueError('bench config is missing required field "request_cpus"')
+    # optional fields
+    macro_default = lambda k,d : d if not k in bench_config else bench_config[k]
+    runsolver_path = macro_default('runsolver_path', 'runsolver')
+    mem_lines = macro_default('mem_lines', DEFAULT_N_MEM_LINES)
+    cpu_per_node = macro_default('cpu_per_node', DEFAULT_N_CPUS)
+    partition = macro_default('partition', DEFAULT_PARTITION)
+    n_runs = macro_default('runs', 1)
+    runsolver_kill_delay = macro_default('runsolver_kill_delay', DEFAULT_RUN_SOLVER_KILL_DELAY)
+    time_buffer = macro_default('slurm_time_buffer', DEFAULT_TIME_BUFFER)
+    exec_path = macro_default('executable', None)
+    timeout_factor = macro_default('timeout_factor', 1)
 
     working_dir = None
     if 'working_dir' in bench_config:
         working_dir = os.path.relpath(str(bench_config['working_dir']), start=Path.home())
 
-    if 'runsolver_kill_delay' in bench_config:
-        runsolver_kill_delay = bench_config['runsolver_kill_delay']
-    else:
-        runsolver_kill_delay = DEFAULT_RUN_SOLVER_KILL_DELAY
-    if 'slurm_time_buffer' in bench_config:
-        time_buffer = int(bench_config['slurm_time_buffer'])
-    else:
-        time_buffer = DEFAULT_TIME_BUFFER
-    
-    if 'instances' in bench_config:
-        instances_file = bench_config['instances']
-    else:
-        raise ValueError('bench config is missing required field "instances"')
-
-    if 'configs' in bench_config:
-        configs_file = bench_config['configs']
-    else:
-        raise ValueError('bench config is missing required field "configs"')
-
-    cpus = int(math.ceil(request_cpu / (cpu_per_node / mem_lines)) * (cpu_per_node / mem_lines))
-
-    if 'executable' in bench_config:
-        exec_path = bench_config['executable']
-    else:
-        exec_path = None
-
-    if 'timeout_factor' in bench_config:
-        timeout_factor = bench_config['timeout_factor']
-    else:
-        timeout_factor = 1
-
     if 'initial_seed' in bench_config:
         random.seed(bench_config['initial_seed'])
+    
+    cpus = int(math.ceil(request_cpu / (cpu_per_node / mem_lines)) * (cpu_per_node / mem_lines))
 
     instances = {}
     with open(instances_file, 'r') as file:
@@ -197,7 +164,7 @@ def main(cobra_config : Dict[str, str | int], bench_config : Dict[str, str | int
                 os.chmod(job_path, st.st_mode | stat.S_IEXEC)
                 counter += 1
     
-    with open(f'{bench_name}/batch_job.slurm', 'w') as file:
+    with open(Path(bench_name, 'batch_job.slurm'), 'w') as file:
         file.write('#!/bin/bash\n')
         file.write('#\n')
         file.write(f'#SBATCH --job-name={bench_name}\n')
@@ -209,22 +176,18 @@ def main(cobra_config : Dict[str, str | int], bench_config : Dict[str, str | int
         file.write(f'#SBATCH --error={bench_name}.log\n')
         file.write(f'#SBATCH --array=0-{counter - 1}\n')
         file.write('#SBATCH --ntasks=1\n\n')
+        file.write(f'cd ~/{os.path.relpath(os.curdir, start=Path.home())}\n')
         file.write(f'FILES=(config*/instance*/run*/start.sh)\n\n')
         file.write('srun ${FILES[$SLURM_ARRAY_TASK_ID]}\n')
         
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        cobra_config_file, bench_config_file = sys.argv[1:]
-        with open(cobra_config_file, 'r') as file:
-            cobra_config = json.loads(file.read())
-    elif len(sys.argv) == 2:
+    if len(sys.argv) == 2:
         bench_config_file = sys.argv[1]
-        cobra_config = {}
     else:
-        print("usage: cobrabench.py [cobra_config_file] <bench_config_file>")
+        print("usage: cobrabench.py <bench_config_file>")
         sys.exit(1)
     
     with open(bench_config_file, 'r') as file:
-        bench_config = json.loads(file.read())
+        bench_config : BenchConfig = json.loads(file.read())
 
-    main(cobra_config, bench_config)
+    main(bench_config)

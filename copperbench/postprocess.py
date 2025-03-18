d682e2084a66ec8e291f56ec801bd38031219a52
Tobias Geibinger <tobias.geibinger@tuwien.ac.at>
2025-03-18 01:15:36 +0100

    improved skipping of completed runs, added job id to node log

--- copperbench/postprocess.py
+++ copperbench/postprocess.py
@@ -17,7 +17,7 @@ def process_bench(bench_folder: Union[Path, str], log_read_func: Callable[[Path]
     else:
         metadata = None
 
-    regex_slurm = re.compile(r"Date:\s+(?P<slurm_date>.+)\nNode:\s+(?P<slurm_node>.+)\n(?s:.)*Cpus_allowed:\s+(?P<slurm_cpumask>.+)\nresctrl cache mask:\s+(?P<slurm_cachemask>.+)")
+    regex_slurm = re.compile(r"Date:\s+(?P<slurm_date>.+)\nNode:\s+(?P<slurm_node>.+)\n(?s:.)*Cpus_allowed:\s+(?P<slurm_cpumask>.+)\nresctrl cache mask:\s+(?P<slurm_cachemask>.+)\nSlurm Job ID:\s+(?P<slurm_jobid>.+)")
     regex_runsolver = re.compile(r"(?s:.*)Max\. virtual memory \(cumulated for all children\) \(KiB\): (?P<runsolver_max_virt_mem_kb>\d+)\nMax\. memory \(cumulated for all children\) \(KiB\): (?P<runsolver_max_mem_kb>\d+)(?s:.*)user time used= (?P<runsolver_user_time>.+)\nsystem time used= (?P<runsolver_system_time>.+)\nmaximum resident set size= (?P<runsolver_max_rss>\d+)")
 
     data = []
@@ -27,13 +27,19 @@ def process_bench(bench_folder: Union[Path, str], log_read_func: Callable[[Path]
                 if instance_dir.name.startswith('instance') and os.path.isdir(instance_dir):
                     for run_dir in os.scandir(instance_dir):
                         if run_dir.name.startswith('run') and os.path.isdir(run_dir):
-                            result_log = log_read_func(Path(run_dir, 'stdout.log'))
-                            result_err = err_read_func(Path(run_dir, 'stderr.log'))
                             result = {}
-                            if result_log:
-                                result = result | result_log
-                            if result_err:
-                                result = result | result_err
+                            
+                            stdout_log = Path(run_dir, 'stdout.log')
+                            if stdout_log.exists():
+                                result_log = log_read_func(stdout_log)
+                                if result_log:
+                                    result = result | result_log
+                                    
+                            stderr_log = Path(run_dir, 'stderr.log')
+                            if stderr_log.exists():
+                                result_err = log_read_func(stderr_log)
+                                if result_err:
+                                    result = result | result_err                            
                             
                             if metadata != None:
                                 conf_name = metadata['configs'][config_dir.name].strip()
@@ -49,31 +55,35 @@ def process_bench(bench_folder: Union[Path, str], log_read_func: Callable[[Path]
                             entry['run'] = run_dir.name[3:]
 
                             if include_metrics:
-                                    with open(Path(run_dir, 'node_info.log'), 'r') as file:
+                                node_log = Path(run_dir, 'node_info.log')
+                                if node_log.exists():
+                                    with open(node_log, 'r') as file:
                                         match = regex_slurm.match(file.read())
                                         if match != None:
                                             entry = entry | match.groupdict()
-                                    with open(Path(run_dir, 'runsolver.log'), 'r') as file:
+                                runsolver_log = Path(run_dir, 'runsolver.log')
+                                if runsolver_log.exists():
+                                    with open(runsolver_log, 'r') as file:
                                         match = regex_runsolver.match(file.read())
                                         if match != None:
                                             entry = entry | match.groupdict()
-                                    perf_log = Path(run_dir, 'perf.log')
-                                    if perf_log.exists():
-                                        with open(perf_log, 'r') as file:
-                                            lines = [ l.strip() for l in file.readlines() ]
-                                            lines = [ l for l in lines if len(l) > 0 ]
-                                            events = lines[2:-3]
-                                            times = lines[-3:]
-                                            for event in events:
-                                                split = [ e for e in event.split(' ') if len(e) > 0 ]
-                                                value = int(split[0].replace(",", "").replace(".", ""))
-                                                variable = split[1]
-                                                entry[f'perf_{variable}'] = value
-                                            for time in times:
-                                                t = time.split(' ')
-                                                value = float(t[0].replace(",", "."))
-                                                variable = '-'.join(t[1:])
-                                                entry[f'perf_{variable}'] = value
+                                perf_log = Path(run_dir, 'perf.log')
+                                if perf_log.exists():
+                                    with open(perf_log, 'r') as file:
+                                        lines = [ l.strip() for l in file.readlines() ]
+                                        lines = [ l for l in lines if len(l) > 0 ]
+                                        events = lines[2:-3]
+                                        times = lines[-3:]
+                                        for event in events:
+                                            split = [ e for e in event.split(' ') if len(e) > 0 ]
+                                            value = int(split[0].replace(",", "").replace(".", ""))
+                                            variable = split[1]
+                                            entry[f'perf_{variable}'] = value
+                                        for time in times:
+                                            t = time.split(' ')
+                                            value = float(t[0].replace(",", "."))
+                                            variable = '-'.join(t[1:])
+                                            entry[f'perf_{variable}'] = value
                                         
                             data += [entry | result]
 
